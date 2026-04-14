e97f33e6ff2cb5912dd2502434ae934c68eab45f
ronniej <ronniej@naver.com>
2026-04-14 11:06:09 +0900

    11강 구조체 생성 및 배열에 사용하는 코드 실습

--- Source/UEPart1/MyGameInstance.cpp
+++ Source/UEPart1/MyGameInstance.cpp
@@ -2,98 +2,62 @@
 
 
 #include "MyGameInstance.h"
-#include "Student.h"
-#include "Teacher.h"
-#include "Staff.h"
-#include "Card.h"
-#include "CourseInfo.h"
-#include "Algo/Accumulate.h"
 
-UMyGameInstance::UMyGameInstance()
+// 이름 자동완성 해주는 함수.
+FString MakeRandomName()
 {
-	// 기본 값은 CDO라는 특별한 템플릿 객체에 저장됨.
-	SchoolName = TEXT("기본학교");
+	// 3글자.
+	TCHAR FirstChar[] = TEXT("김이박최");
+	TCHAR MiddleChar[] = TEXT("상혜지성");
+	TCHAR LastChar[] = TEXT("수은원연");
+
+	// 동적 배열을 사용할 때 가능하다면 재할당을 방지하는게 좋음.
+	TArray<TCHAR> RandArray;
+	RandArray.SetNum(3);
+	RandArray[0] = FirstChar[FMath::RandRange(0, 3)];
+	RandArray[1] = MiddleChar[FMath::RandRange(0, 3)];
+	RandArray[2] = LastChar[FMath::RandRange(0, 3)];
+
+	// 문자열로 변환이 가능하도록 반환.
+	return RandArray.GetData();
 }
 
 void UMyGameInstance::Init()
 {
 	Super::Init();
 
-	// TArray 사용.
-	const int32 ArrayNum = 10;
-	TArray<int32> Int32Array;
-
-	for (int32 ix = 1; ix <= ArrayNum; ++ix)
+	// 학생 이름 데이터 생성.
+	const int32 StudentNum = 300;
+	for (int32 ix = 1; ix <= StudentNum; ++ix)
 	{
-		Int32Array.Add(ix);
+		StudentsData.Emplace(
+			FStudentData(MakeRandomName(), ix)
+		);
 	}
-
-	// 짝수 제거.
-	Int32Array.RemoveAll(
-		// [] - 캡처 (외부 내용을 람다 안에서 사용할 때 활용).
-		// () - 파라미터.
-		// -> - 반환형.
-		// { } - 본문
-		[](int32 Val) -> bool
-		{
-			return Val % 2 == 0;
-		}
-	);
-
-	// 짝수 삽입.
-	Int32Array += { 2, 4, 6, 8, 10};
-
-	// 비교 ( 동등 비교 ).
-	TArray<int32> Int32ArrayCompare;
-	int32 CArray[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
-	Int32ArrayCompare.AddUninitialized(ArrayNum);
-	
-	// C 스타일 배열을 TArray에 메모리 복사.
-	FMemory::Memcpy(
-		Int32ArrayCompare.GetData(), 
-		CArray, 
-		sizeof(int32) * ArrayNum
-	);
-
-	// 어서트 (크래시를 발생시키지 않고, 출력 로그 창에 오류 메시지 출력).
-	ensureAlways(Int32Array == Int32ArrayCompare);
-
-	// 합계.
-	int32 Sum = 0;
-	for (const int32& Int32Num : Int32Array)
-	{
-		Sum += Int32Num;
-	}
-
-	// 알고리즘 활용 (합계 구하기).
-	int32 SumByAlgo = Algo::Accumulate(Int32Array, 0);
-	ensureAlways(Sum == SumByAlgo);
 
 	UE_LOG(
 		LogTemp,
 		Log,
-		TEXT("Sum = %d | SumByAlgo: %d | Sum == SumByAlgo: %s"),
-		Sum, SumByAlgo, ( Sum == SumByAlgo ? TEXT("True") : TEXT("False") )
+		TEXT("모든 학생 데이터의 수: %d"),
+		StudentsData.Num()
 	);
 
-	// TSet 사용.
-	TSet<int32> Int32Set;
-	for (int32 ix = 1; ix <= ArrayNum; ++ix)
-	{
-		Int32Set.Add(ix);
-	}
+	// 학생 데이터에서 이름 값만 추출해서 배열에 저장.
+	TArray<FString> AllStudentNames;
+	// 알고리즘을 활용해서 이름 값 추출.
+	Algo::Transform(
+		StudentsData,
+		AllStudentNames,
+		[](const FStudentData& Val)
+		{
+			return Val.Name;
+		}
+	);
 
-	// 제거.
-	Int32Set.Remove(2);
-	Int32Set.Remove(4);
-	Int32Set.Remove(6);
-	Int32Set.Remove(8);
-	Int32Set.Remove(10);
-
-	// 추가.
-	Int32Set.Add(2);
-	Int32Set.Add(4);
-	Int32Set.Add(6);
-	Int32Set.Add(8);
-	Int32Set.Add(10);
+	UE_LOG(
+		LogTemp,
+		Log,
+		TEXT("모든 학생 이름의 수: %d"),
+		AllStudentNames.Num()
+	);
 }
---
+++ Source/UEPart1/StudentData.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StudentData.generated.h"
+
+USTRUCT(/*BlueprintType, Atomic*/)
+struct FStudentData
+{
+	GENERATED_BODY()
+
+	FStudentData()
+	{
+		Name = TEXT("기본이름");
+		Order = -1;
+	}
+
+	FStudentData(const FString& InName, int32 InOrder)
+		: Name(InName), Order(InOrder)
+	{
+	}
+
+	UPROPERTY()
+	FString Name;
+
+	UPROPERTY()
+	int32 Order;
+};
