39a740cba7481309d641992f0ee6aba64a214a0d
ronniej <ronniej@naver.com>
2026-04-14 15:56:38 +0900

    12강 언리얼 메모리 관리 수업 진행
    - 프로젝트 세팅 > Garbage Collection에서 가비지 컬렉터 동작 주기 조정 (3초)
    - UPROPERTY 지정한 UObject와 지정하지 않은 UObject 변수에 대한 유효성 테스트

---
+++ Source/UEPart1/StudentManager.cpp
@@ -0,0 +1,13 @@
+﻿#include "StudentManager.h"
+#include "Student.h"
+
+void FStudentManager::AddReferencedObjects(
+	FReferenceCollector& Collector)
+{
+	// 유효성 확인 후 지정.
+	//IsValid()
+	if (SafeStudent->IsValidLowLevel())
+	{
+		Collector.AddReferencedObject(SafeStudent);
+	}
+}
